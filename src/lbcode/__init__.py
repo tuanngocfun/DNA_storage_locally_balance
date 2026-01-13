b58a1ffb3779e165666b9277ffe9dbe03d4b867d
tuanngocfun <tuanngoccs50@gmail.com>
2026-01-13 11:25:26 +0700

    feat(init): add initial implementation of LBCode library with M4 verifier

---
+++ src/lbcode/__init__.py
@@ -0,0 +1,12 @@
+"""
+LBCode - Locally Balanced Constraint Coding Library
+Based on Ge22 Paper: "Coding for Locally Balanced Constraints"
+
+M4 Verifier Implementation
+"""
+
+from .verifier import is_locally_balanced, is_rll
+from .graph_alg1 import algorithm1_find_core, CoreResult
+
+__version__ = "1.0.0"
+__all__ = ["is_locally_balanced", "is_rll", "algorithm1_find_core", "CoreResult"]
